127d799b0209080db82b5a04953a269a5cf09dea
Marlon Mendes <marlonciriatico@gmail.com>
2018-06-22 00:50:19 -0300

    Refactoring views

--- github_oauth/home/views.py
+++ github_oauth/home/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render
-import github
 from github import Github
 
-def debug(a, b):
-    print('{} = {}'.format(a, b))
-    print('type({}) = {}'.format(a, type(b)))
-
-def debug_dict(a, b):
-    print('\n({}) = '.format(a))
-    for key, value in b.items():
-        print('[{}] = {}'.format(key, value))
 
 def get_access_token(request):
     user = request.user
@@ -37,39 +28,6 @@ def organizations(request):
     user = g.get_user()
     orgs = user.get_orgs()
     orgs_names = [org.login for org in orgs]
-    debug('names', orgs_names)
     context = {'header': 'My organizations',
                'rows': orgs_names}
     return render(request, 'list_base.html', context=context)
-
-def get_user_organizations(user):
-    assert isinstance(user, github.AuthenticatedUser.AuthenticatedUser)
-
-    orgs = []
-    obj = user.get_orgs()
-    if isinstance(obj, github.PaginatedList.PaginatedList):
-        orgs += [org for org in obj]
-    elif isinstance(obj, github.Organization.Organization):
-        orgs += [obj]
-    else:
-        raise Exception
-    return orgs
-
-def get_repos(request):
-    token = get_access_token(request)
-    g = Github(token)
-    debug('g', g)
-
-    user = g.get_user()
-
-    print('\n')
-    for repo in user.get_repos():
-        print('repo.name = {}'.format(repo.full_name))
-
-    #orgs = get_user_organizations(user)
-    orgs = g.get_organizations()
-
-    for org in orgs:
-        debug('org', org)
-        print('url = {}'.format(org.url))
-        print('\n')
